4e1eb5056876c99c3d534c4b72b55023af59a7da
Xavier Li <xavierli4ca@gmail.com>
2023-03-01 10:43:26 -0500

    Initial commit

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+/* 
+  File Name:     /controller/user.js
+  Student Name:  Yuen Kwan LI
+  Student ID:    301228849
+  Date:          1-MAR-2023 
+*/
+
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+
+  "URI" : "mongodb+srv://root:HayUxOC1rzhxzfGw@cluster0.r33ip38.mongodb.net/books229?retryWrites=true&w=majority"
+};
